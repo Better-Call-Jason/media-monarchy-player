22aa3b4497770fbf1bff6db32aa5f823de3a3c32
Better-Call-Jason <145218541+Better-Call-Jason@users.noreply.github.com>
2025-09-06 18:25:54 -0400

    Update media-monarchy-player.php

--- media-monarchy-player.php
+++ media-monarchy-player.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Media Monarchy 24/7 Radio Stream
 Description: A streaming media player that supports 8 distinct streams: use 'onair' for live broadcasts, 'news' for all news content, 'stream' for all music content, and genre-specific streams including 'rock', 'techno', 'country', 'ecclectic', and 'pop'. Example shortcode: [media_monarchy stream="country"]
-Version: 1.3.1
+Version: 1.4.1
 Author: Better Call Jason
 */
 
@@ -23,7 +23,7 @@ function media_monarchy_player_shortcode($atts) {
     $atts = shortcode_atts(array(
         'stream' => 'music',
         'theme' => 'dark',
-        'width' => '99%',
+        'width' => '100%',
         'height' => '63%'
     ), $atts);
     
