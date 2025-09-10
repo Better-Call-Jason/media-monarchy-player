8bd7df0fbd39d03180b41aba54a8f9d1f8206678
Better-Call-Jason <145218541+Better-Call-Jason@users.noreply.github.com>
2025-09-10 09:32:58 -0400

    Update media-monarchy-player.php

--- media-monarchy-player.php
+++ media-monarchy-player.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Media Monarchy 24/7 Radio Stream
 Description: A streaming media player that supports 8 distinct streams: use 'onair' for live broadcasts, 'news' for all news content, 'stream' for all music content, and genre-specific streams including 'rock', 'techno', 'country', 'ecclectic', and 'pop'. Example shortcode: [media_monarchy stream="country"]
-Version: 1.4.1
+Version: 1.4.2
 Author: Better Call Jason
 */
 
@@ -24,10 +24,12 @@ function media_monarchy_player_shortcode($atts) {
         'stream' => 'music',
         'theme' => 'dark',
         'width' => '100%',
-        'height' => '63%'
+        'height' => '63%',
+        'volume' => '50' 
     ), $atts);
     
     $id = 'mm-player-' . uniqid();
+    $volume = intval($atts['volume']) / 100;
     
     return sprintf(
         '<div id="%s"></div>
@@ -37,11 +39,12 @@ function media_monarchy_player_shortcode($atts) {
                 stream: "%s",
                 theme: "%s",
                 width: "%s",
-                height: "%s"
+                height: "%s",
+                volume: %s
             });
         });
         </script>',
-        $id, $id, $atts['stream'], $atts['theme'], $atts['width'], $atts['height']
+        $id, $id, $atts['stream'], $atts['theme'], $atts['width'], $atts['height'], volume
     );
 }
 add_shortcode('media_monarchy', 'media_monarchy_player_shortcode');
